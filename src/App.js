ca575b3a8b6a689242e22e0f07604ef10b4983af
manjari jain <manjarijain98@gmail.com>
2021-04-12 17:05:27 +0530

    adding styles to ipod

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="I-Pod">
+            <div className="screen">
+
+            </div>
+            <div className="button-set">
+                <div className="button-set-text menu">Menu</div>
+                <img className="button-set-text forward" src="https://www.flaticon.com/svg/vstatic/svg/660/660276.svg?token=exp=1618227330~hmac=6bb976f2403fe46075301cfe3d845fa5" />
+                <img className="button-set-text backward" src="https://www.flaticon.com/svg/vstatic/svg/39/39712.svg?token=exp=1618227574~hmac=fcedf4aceb03fb55a88c03d3f55c4ed7"/>
+                <button className="inner-circle">
+
+                </button>
+                <img className="button-set-text play" src="https://www.flaticon.com/svg/vstatic/svg/27/27185.svg?token=exp=1618227794~hmac=8adddeac8cfc252084b882f13a4ff0dc"/>
+
+            </div>
+        </div>
     </div>
   );
 }
