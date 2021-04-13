d4a5c17a69fed09f10f077f23030b99cd4a69e99
manjari jain <manjarijain98@gmail.com>
2021-04-13 17:40:19 +0530

    improved styles

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
+import React from 'react';
+import ZingTouch from 'zingtouch';
+import $ from 'jquery';
+
+class App extends React.Component {
+
+  constructor()
+  {
+    super();
+    this.state={
+      menuDisplay:false,
+    }
+  }
+
+  menuClicked=()=>{
+    const {menuDisplay}=this.state;
+    if(!menuDisplay)
+    {
+      $(".small-screen").addClass('menuToggle');
+        this.setState({
+          menuDisplay:true
+        });
+    }
+    else
+    {
+      $(".small-screen").removeClass('menuToggle');
+      this.setState({
+        menuDisplay:false
+      });
+    }
+  }
 
 
-function App() {
-  return (
-    <div className="App">
-        <div className="I-Pod">
-            <div className="screen">
+  render()
+  {
+    return (
+      <div className="App">
+          <div className="I-Pod">
+              <div className="screen">
 
-            </div>
-            <div className="button-set">
-                <div className="button-set-text menu">Menu</div>
-                <img className="button-set-text forward" src="https://www.flaticon.com/svg/vstatic/svg/660/660276.svg?token=exp=1618227330~hmac=6bb976f2403fe46075301cfe3d845fa5" />
-                <img className="button-set-text backward" src="https://www.flaticon.com/svg/vstatic/svg/39/39712.svg?token=exp=1618227574~hmac=fcedf4aceb03fb55a88c03d3f55c4ed7"/>
-                <button className="inner-circle">
+                <div className="small-screen">
 
-                </button>
-                <img className="button-set-text play" src="https://www.flaticon.com/svg/vstatic/svg/27/27185.svg?token=exp=1618227794~hmac=8adddeac8cfc252084b882f13a4ff0dc"/>
+                </div>
+  
+              </div>
+              <div className="button-set">
+                  <button className="button-set-text menu" onClick={this.menuClicked}>Menu</button>
+                  <button className="button-set-text forward"> <i className="fas fa-forward"></i> </button>
+                  <button className="button-set-text backward"><i className="fas fa-backward"></i></button>
+                  <button className="inner-circle">
 
-            </div>
-        </div>
-    </div>
-  );
+                  </button>
+                  <button className="button-set-text play"><i className="fas fa-play"></i><i className="fas fa-pause"></i></button>
+  
+              </div>
+          </div>
+      </div>
+    );
+  }
 }
 
 export default App;
