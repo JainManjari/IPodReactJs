297d82542730dfaf4a631fe67933d37b8b2c0900
manjari jain <manjarijain98@gmail.com>
2021-04-14 15:50:07 +0530

    zingtouch

--- src/App.js
+++ src/App.js
@@ -7,9 +7,50 @@ class App extends React.Component {
   constructor()
   {
     super();
+    this.temp_change_in_angle=0;
+    this.temp_selected=0;
     this.state={
       menuDisplay:false,
+      options:['Games','Music','Settings','Coverflow'],
+      general_menu:['Games','Music','Settings','Coverflow'],
+      selected:0,
     }
+  }
+  
+  componentDidMount()
+  {
+      var zt = new ZingTouch.Region(document.getElementsByClassName('button-set')[0]);
+      zt.bind(document.getElementsByClassName('button-set')[0], 'rotate', (event) =>
+      {
+          if (document.getElementsByClassName('small-screen')[0].classList.contains('menuToggle'))//this rotating facility will only be available when the side bar is shown to the user.
+          {
+              let dist = event.detail.distanceFromLast;
+              this.temp_change_in_angle += dist;
+              if (this.temp_change_in_angle > 60)
+              {
+                  this.temp_selected++;
+                  this.temp_selected = this.temp_selected % this.state.options.length;
+                  this.setState({
+                      selected: this.temp_selected,
+                  });
+
+                  this.temp_change_in_angle = 0;
+              }
+              else if (this.temp_change_in_angle < -60)
+              {
+                  this.temp_selected--;
+                  if (this.temp_selected === -1)
+                      this.temp_selected = this.state.options.length-1;
+
+                  this.temp_selected = this.temp_selected % this.state.options.length;
+                  this.setState({
+                      selected: this.temp_selected,
+                  });
+                  this.temp_change_in_angle = 0;
+              }
+          }
+
+      });
   }
 
   menuClicked=()=>{
@@ -33,12 +74,27 @@ class App extends React.Component {
 
   render()
   {
+    const {general_menu,selected}=this.state;
     return (
       <div className="App">
           <div className="I-Pod">
               <div className="screen">
 
                 <div className="small-screen">
+                    <span className="title">MadJerry's IPod!</span> 
+                    
+                    <div className="list-items">
+
+                        {general_menu.map((item,index)=>{
+                            return (
+                            <button 
+                            className={selected===index ? 'selected' : ''} 
+                            key={index}>
+                              {item}
+                            </button>); 
+                        })}
+
+                    </div>
 
                 </div>
   
