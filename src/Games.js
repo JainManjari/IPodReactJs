265b47785421de375c29f24745e8be8e14d86bb0
manjari jain <manjarijain98@gmail.com>
2021-04-14 17:39:17 +0530

    add styles to music and games

--- src/Games.js
+++ src/Games.js
@@ -3,7 +3,13 @@ const Games=()=>{
     return(
         <div className="game-screen">
 
-            <h1>Games</h1>
+            <h1 className="game-title">Games</h1>
+
+            <div className="game-icons">
+                <i className="fab fa-wizards-of-the-coast"></i>
+                <i className="fas fa-gamepad"></i>
+                <i className="fas fa-chess-queen"></i>
+            </div>
 
         </div>
     );
--- src/Music.js
+++ src/Music.js
@@ -3,7 +3,13 @@ const Music=()=>{
     return(
         <div className="music-screen">
 
-            <h1>Music</h1>
+            <h1 className="music-title">Music</h1>
+
+            <div className="music-icons">
+                <i className="fas fa-drum"></i>
+                <i className="fas fa-guitar"></i>
+                <i className="fab fa-napster"></i>
+            </div>
 
         </div>
     );
